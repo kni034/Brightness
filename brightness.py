6af87b9cb4cb5b5807a219e042617b57e531867a
Kristoffer Borg Nilsen <43201432+kni034@users.noreply.github.com>
2020-09-18 16:06:41 +0200

    Add files via upload

---
+++ brightness.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import subprocess
+import sys
+
+
+input = sys.argv[1]
+print(type(input))
+
+try:
+    if str(input) in light_levels:
+        brightness = ligh_levels[input.lower()]
+    elif float(input) > 0.0 and float(input) <= 1.0:
+        brightness = input
+    else:
+        raise Exception("wrong argument")
+except:
+    print("Give brightness argument 0.0 - 1")
+    sys.exit(1)
+
+
+
+get_screens = 'xrandr | grep " connected" | cut -f1 -d " "'
+
+p1 = subprocess.Popen(get_screens, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+screens = p1.communicate()[0]
+
+screen_list = screens.splitlines()
+
+for screen in screen_list:
+    subprocess.run(['xrandr', '--output', screen, '--brightness', str(brightness)])
